87b4964ce92e7799e43f7ade6cef597c32352dbf
AnnieM101 <annacarolinekatherine@gmail.com>
2025-08-22 05:44:38 -0400

    Updated handleErrors controller
    -Added new error handling for 409 duplicate email errors

--- utils/errors.js
+++ utils/errors.js
@@ -2,25 +2,40 @@ const STATUS_CODE = {
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
   BAD_REQUEST: 400,
+  DUPLICATE_EMAIL: 409,
 };
 
 const ERROR_MESSAGE = {
   NOT_FOUND: "Requested resource not found",
   INTERNAL_SERVER_ERROR: "An unexpected error occurred",
   BAD_REQUEST: "Invalid request data",
+  DUPLICATE_EMAIL: "Email already in use",
 };
 const errorHandling = (res, req, err) => {
   console.warn(err);
   if (err.name === "CastError") {
-    return res.status(STATUS_CODE.BAD_REQUEST).send({ message : ERROR_MESSAGE.BAD_REQUEST });
+    return res
+      .status(STATUS_CODE.BAD_REQUEST)
+      .send({ message: ERROR_MESSAGE.BAD_REQUEST });
   }
   if (err.name === "ValidationError") {
-    return res.status(STATUS_CODE.BAD_REQUEST).send({ message : ERROR_MESSAGE.BAD_REQUEST });
+    return res
+      .status(STATUS_CODE.BAD_REQUEST)
+      .send({ message: ERROR_MESSAGE.BAD_REQUEST })
   }
+   if (err.code === 11000) {
+      return res
+        .status(STATUS_CODE.DUPLICATE_EMAIL)
+        .send({ message: ERROR_MESSAGE.DUPLICATE_EMAIL });
+    }
   if (err.name === "DocumentNotFoundError") {
-    return res.status(STATUS_CODE.NOT_FOUND).send({ message: ERROR_MESSAGE.NOT_FOUND});
+    return res
+      .status(STATUS_CODE.NOT_FOUND)
+      .send({ message: ERROR_MESSAGE.NOT_FOUND })
   } else {
-    return res.status(STATUS_CODE.INTERNAL_SERVER_ERROR).send({ message : ERROR_MESSAGE.INTERNAL_SERVER_ERROR});
+    return res
+      .status(STATUS_CODE.INTERNAL_SERVER_ERROR)
+      .send({ message: ERROR_MESSAGE.INTERNAL_SERVER_ERROR });
   }
 };
 
