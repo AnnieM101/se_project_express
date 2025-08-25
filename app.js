463f8f3255694dad9f559c124c634f2f6f294358
AnnieM101 <annacarolinekatherine@gmail.com>
2025-08-24 22:29:54 -0400

    Installed cors
    Installed cors and removed repeat line

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const clothingRouter = require("./routes/clothingItems");
-const cors = require('cors');
+const cors = require("cors");
 
 const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
